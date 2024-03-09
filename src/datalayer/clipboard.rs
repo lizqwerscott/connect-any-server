1ce54d6bd857d120dae80a22c1c179b72f394fcb
lizqwerscott <lizqwerscott@163.com>
2024-03-09 22:50:36 +0800

    feat: parse unix timestamp

--- src/datalayer/clipboard.rs
+++ src/datalayer/clipboard.rs
@@ -1,6 +1,7 @@
 use std::fmt;
 use std::time::{SystemTime, UNIX_EPOCH};
 
+use chrono::{DateTime, Local};
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
@@ -43,6 +44,10 @@ impl Clipboard {
 impl fmt::Display for Clipboard {
     // 这个 trait 要求 `fmt` 使用与下面的函数完全一致的函数签名
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Clipboard[{}]: {}", self.date, self.data)
+        let duration = UNIX_EPOCH + std::time::Duration::from_millis(self.date as u64);
+        let datetime = DateTime::<Local>::from(duration);
+        let formatted_datetime = datetime.format("%Y-%m-%d %H:%M:%S").to_string();
+
+        write!(f, "Clipboard[{}]: {}", formatted_datetime, self.data)
     }
 }
